5ccd0fc9b7981fc5d2fec322e79a4d8f1ff8985e
Deepak Jois <deepak.jois@gmail.com>
2024-08-22 10:28:20 +0530

    Remove glob for go files from watch

--- watch.sh
+++ watch.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-fswatch -0 -o content static templates | xargs -0 -n1 -I{} sh -c 'go build && ./debugjois.dev build --dev'
+fswatch -0 -o content static templates *.go | xargs -0 -n1 -I{} sh -c 'go build && ./debugjois.dev build --dev'
