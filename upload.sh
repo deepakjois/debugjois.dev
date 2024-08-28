d7dc155c2aaaef9f799889c3a58ce51fdf23bc95
Deepak Jois <deepak.jois@gmail.com>
2024-08-28 19:46:09 +0530

    Tweak upload script

--- upload.sh
+++ upload.sh
@@ -6,32 +6,9 @@ S3_BUCKET="debugjois-dev-site"
 # local folder path
 LOCAL_FOLDER="build"
 
-# Loop through all files in the local folder
-for file in "$LOCAL_FOLDER"/*; do
-    # Get the filename
-    filename=$(basename "$file")
+aws s3 sync --size-only build/ s3://debugjois-dev-site/ --exclude "index.html" --exclude "daily*" --include "daily.xml"
+aws s3 sync --size-only build/ s3://debugjois-dev-site/ --exclude "*" --include "daily-archive-*" --content-type "text/html"
+aws s3 sync build/ s3://debugjois-dev-site/ --exclude "*" --include "index.html" --include "daily" --exclude "daily.xml" --content-type "text/html"
 
-    # Get the file extension
-    extension="${filename##*.}"
-
-    # Set the content type
-    if [ "$extension" = "$filename" ]; then
-        # No extension, set MIME type to text/html
-        content_type="text/html"
-    else
-        # Use the default content type based on the file extension
-        content_type=""
-    fi
-
-    # Upload the file to S3
-    if [ -n "$content_type" ]; then
-        aws s3 cp "$file" "s3://$S3_BUCKET/$filename" --content-type "$content_type"
-    else
-        aws s3 cp "$file" "s3://$S3_BUCKET/$filename"
-    fi
-done
-
-# Sync the images folder
-aws s3 sync "build/images" "s3://$S3_BUCKET/images"
 
 echo "Upload complete!"
