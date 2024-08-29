afcf6024a8b994ea02e4b858000e074b32372b9b
Deepak Jois <deepak.jois@gmail.com>
2024-08-29 10:10:14 +0530

    Tweak upload.sh

--- upload.sh
+++ upload.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 aws s3 sync --size-only build/ s3://debugjois-dev-site/ --exclude "index.html" --exclude "daily*" --include "daily.xml"
 aws s3 sync --size-only build/ s3://debugjois-dev-site/ --exclude "*" --include "daily-archive-*" --content-type "text/html"
-aws s3 sync build/ s3://debugjois-dev-site/ --exclude "*" --include "index.html" --include "daily" --exclude "daily.xml" --content-type "text/html"
+aws s3 sync build/ s3://debugjois-dev-site/ --exclude "*" --include "index.html" --include "daily" --include "daily.xml" --content-type "text/html"
+
+
 echo "Upload complete!"
