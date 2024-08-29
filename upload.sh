5517026f83f5494296c93466eced9a3e5ebb9aa6
Deepak Jois <deepak.jois@gmail.com>
2024-08-28 21:36:37 -0700

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
-
-# S3 bucket name
-S3_BUCKET="debugjois-dev-site"
-
-# local folder path
-LOCAL_FOLDER="build"
-
 aws s3 sync --size-only build/ s3://debugjois-dev-site/ --exclude "index.html" --exclude "daily*" --include "daily.xml"
 aws s3 sync --size-only build/ s3://debugjois-dev-site/ --exclude "*" --include "daily-archive-*" --content-type "text/html"
 aws s3 sync build/ s3://debugjois-dev-site/ --exclude "*" --include "index.html" --include "daily" --exclude "daily.xml" --content-type "text/html"
-
-
 echo "Upload complete!"
