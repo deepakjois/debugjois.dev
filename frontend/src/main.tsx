5c8731b4f009dbbd7b391b9554cea204f95cc599
Deepak Jois <deepak.jois@gmail.com>
2026-03-10 19:29:16 +0100

    fix styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createRouter, RouterProvider } from "@tanstack/react-router";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { routeTree } from "./routeTree.gen";
+import "./global.css";
 
 const queryClient = new QueryClient({
   defaultOptions: {
